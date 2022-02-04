97da51fa8a2c238cfe1c78294bc2e20ae897a0a6
Ross <rossllewellyn95@hotmail.co.uk>
2022-02-04 08:52:45 +0000

    Added change of game state

--- app/pages/quiz.tsx
+++ app/pages/quiz.tsx
@@ -25,6 +25,7 @@ const Quiz: NextPage = () => {
   const [question, setQuestion] = useState('Is this the question?');
   const [answer, setAnswer] = useState('');
   const [correctAnswers, setCorrectAnswers] = useState<string[]>([]);
+  const [correctAnswer, setCorrectAnswer] = useState('');
 
   useEffect(() => {
     // if session exists, reconnect to server
@@ -91,7 +92,8 @@ const Quiz: NextPage = () => {
 
     socket.on('new_question', (questionsAndAnswers) => {
       setQuestion(questionsAndAnswers.question);
-      // setCorrectAnswer(questionsAndAnswers.correctAnswer);
+      setCorrectAnswer(questionsAndAnswers.correctAnswer);
+      setGameState('question');
     });
 
     socket.on('answer_list', (answerList) => {
