5fdd348fbc3a8d531d3ddb87db9f2adcb20a4a7c
Angus Edwards <angus.s.edwards@gmail.com>
2022-02-09 10:47:54 +0000

    added icons

--- app/pages/navbar.tsx
+++ app/pages/navbar.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next';
 import NavbarItem from './components/navbarItem';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser } from '@fortawesome/free-solid-svg-icons'
+import { faBook } from '@fortawesome/free-solid-svg-icons'
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar: NextPage = () => (
     <div className="navbar">
-      <NavbarItem text="Rules" url="/rules" />
-      <NavbarItem text="Home" url="/" />
-      <NavbarItem text="Quiz" url="/quiz" />
+      <NavbarItem text="Rules" icon={faBook} url="/rules" />
+      <NavbarItem text="Home"  icon={faHouseUser} url="/" />
+      <NavbarItem text="Quiz" icon={faCheck} url="/quiz" />
     </div>
 );
 
